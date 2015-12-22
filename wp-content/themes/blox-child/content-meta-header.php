81d60e0a37b2f90ecd8cbe2d1522dc41e68c282c
TiKawamoto <tikawamoto@gmail.com>
2015-12-22 14:30:19 -0500

    Remove publish date

---
+++ wp-content/themes/blox-child/content-meta-header.php
@@ -0,0 +1,5 @@
+<!--BEGIN .entry-meta .entry-header-->
+<div class="entry-meta entry-header">
+	<?php edit_post_link( __('Edit', 'zilla'), '<span class="edit-post">', '</span>' ); ?>
+<!--END .entry-meta entry-header -->
+</div>
