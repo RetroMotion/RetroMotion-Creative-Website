5aa30977619eacb3a69ce12abf36779c40d0190b
TiKawamoto <tikawamoto@gmail.com>
2016-01-22 12:47:19 -0500

    GA Link attribution

--- wp-content/themes/blox-child/header.php
+++ wp-content/themes/blox-child/header.php
@@ -52,6 +52,17 @@
 
 <!-- BEGIN body -->
 <body <?php body_class('no-js'); ?>>
+	
+	<!-- Google Tag Manager -->
+	<noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-PG8W3R"
+	height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+	'//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+	})(window,document,'script','dataLayer','GTM-PG8W3R');</script>
+	<!-- End Google Tag Manager -->
+
     <?php zilla_body_start(); ?>
 
     <div class="header-outer">
