4b9475eb0d0852491fcc77811de6654db3f3b372
Ti <tikawamoto@gmail.com>
2016-05-12 15:40:28 -0400

    form field css

--- wp-content/themes/blox-child/header.php
+++ wp-content/themes/blox-child/header.php
@@ -46,10 +46,6 @@
 	<script src="http://css3-mediaqueries-js.googlecode.com/svn/trunk/css3-mediaqueries.js"></script>
 	<![endif]-->
 	
-
-	<?php wp_head(); ?>
-    <?php zilla_head(); ?>
-   
    	<!-- Latest compiled and minified CSS -->
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 
@@ -59,6 +55,10 @@
 	<!-- Latest compiled and minified JavaScript -->
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
 	
+
+	<?php wp_head(); ?>
+    <?php zilla_head(); ?>
+   
 <!-- END head -->
 </head>
 
