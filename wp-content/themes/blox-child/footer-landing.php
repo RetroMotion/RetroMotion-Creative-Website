958b98288f9d5f96737af6f8c4274cda4f9a861b
Ti <tikawamoto@gmail.com>
2016-05-19 16:45:56 -0400

    Landing Page Custom Theme

---
+++ wp-content/themes/blox-child/footer-landing.php
@@ -0,0 +1,98 @@
+    <?php zilla_content_end(); ?>
+	
+	<script>
+	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+	  ga('create', 'UA-71641373-1', 'auto');
+	  ga('require', 'linkid');
+	  ga('send', 'pageview');
+
+	</script>
+	
+	<!-- Facebook Pixel Code -->
+	<script>
+	!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+	n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
+	n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
+	t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
+	document,'script','//connect.facebook.net/en_US/fbevents.js');
+
+	fbq('init', '957977350948639');
+	fbq('track', "PageView");
+	fbq('track', 'ViewContent');</script>
+	<noscript><img height="1" width="1" style="display:none"
+	src="https://www.facebook.com/tr?id=957977350948639&ev=PageView&noscript=1"
+	/></noscript>
+	<!-- End Facebook Pixel Code -->
+	
+    <?php if( !is_page_template('template-portfolio.php') && !is_singular('portfolio') && !is_post_type_archive( 'portfolio' ) ) { ?>
+		<!-- END #content -->
+		</div>
+	<?php } ?>
+	
+	<!--BEGIN .footer-outer -->
+	<div class="footer-outer footer-landing">
+
+		<?php zilla_footer_before(); ?>
+			
+		<!-- BEGIN #footer -->
+		<div id="footer">
+		    
+		    <?php zilla_footer_start(); ?>
+
+		    <?php zilla_nav_before(); ?>
+		    <?php if( has_nav_menu( 'footer-menu' ) ) {
+		        wp_nav_menu( array( 'theme_location' => 'footer-menu', 'container' => 'div', 'container_id' => 'footer-nav', 'depth' => 1 ) ); 
+		    } ?>
+			<?php zilla_nav_after(); ?>
+			<div class="container-fluid">
+				<div class="row">
+					<div class="col-md-3">
+						<img class="footer-logo" src="../wp-content/uploads/2016/02/rm-creative-white-full-sm-300w.png" width="100">
+				
+						<br>
+						<p class="copyright">© 2016 RetroMotion Creative, LLC</p>
+					</div>
+					<div class="col-md-3">
+					
+					</div>
+					<div class="col-md-3">
+						
+					</div>
+					<div class="col-md-3">
+						<p>Give us a ring</p>
+						<a href="tel:8028467532"><h2><i class="fa fa-phone-square" aria-hidden="true"></i> 802.846.7532</h2></a>
+					</div>
+				</div>
+		    </div>
+			
+			
+		
+		    <?php zilla_footer_end(); ?>
+		    
+		<!-- END #footer -->
+		</div>
+		
+		<?php zilla_footer_after(); ?>
+	
+	<!--END .footer-outer -->	
+	</div>
+	<script>
+	
+		$("[data-toggle=tooltip").tooltip();  
+	
+	</script>
+	<a href="#" id="back-to-top"></a>
+
+	<!-- Theme Hook -->
+	<?php wp_footer(); ?>
+	<?php zilla_body_end(); ?>
+			
+	<!-- <?php echo 'Ran '. $wpdb->num_queries .' queries '. timer_stop(0, 2) .' seconds'; ?> -->
+<!--END body-->
+</body>
+<!--END html-->
+</html>
---
+++ wp-content/themes/blox-child/header-landing.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+
+<!-- BEGIN html -->
+<html <?php language_attributes(); ?>>
+
+<!-- BEGIN head -->
+<head>
+
+	<!-- Meta Tags -->
+	<meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	
+	<!-- Title -->
+	<title><?php wp_title('|', true, 'right'); ?></title>
+	
+	<!-- RSS & Pingbacks -->
+	<link rel="alternate" type="application/rss+xml" title="<?php bloginfo( 'name' ); ?> RSS Feed" href="<?php if(zilla_get_option('general_feedburner_url')){ echo zilla_get_option('general_feedburner_url'); } else { bloginfo( 'rss2_url' ); } ?>" />
+	<link href='https://fonts.googleapis.com/css?family=Roboto:400,100,100italic,300,300italic,400italic,500,500italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
+	<link href='https://fonts.googleapis.com/css?family=Sanchez:400,400italic' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/material-design-iconic-font/2.2.0/css/material-design-iconic-font.min.css">
+	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+	
+	<!--favicon -->
+	<link rel="apple-touch-icon" sizes="57x57" href="/apple-icon-57x57.png">
+	<link rel="apple-touch-icon" sizes="60x60" href="/apple-icon-60x60.png">
+	<link rel="apple-touch-icon" sizes="72x72" href="/apple-icon-72x72.png">
+	<link rel="apple-touch-icon" sizes="76x76" href="/apple-icon-76x76.png">
+	<link rel="apple-touch-icon" sizes="114x114" href="/apple-icon-114x114.png">
+	<link rel="apple-touch-icon" sizes="120x120" href="/apple-icon-120x120.png">
+	<link rel="apple-touch-icon" sizes="144x144" href="/apple-icon-144x144.png">
+	<link rel="apple-touch-icon" sizes="152x152" href="/apple-icon-152x152.png">
+	<link rel="apple-touch-icon" sizes="180x180" href="/apple-icon-180x180.png">
+	<link rel="icon" type="image/png" sizes="192x192"  href="/android-icon-192x192.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+	<link rel="manifest" href="/manifest.json">
+	<meta name="msapplication-TileColor" content="#ffffff">
+	<meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
+	<meta name="msapplication-navbutton-color" content="#f57f25">
+	<meta name="theme-color" content="#f57f25">
+	
+
+	<!--[if lt IE 9]>
+	<script src="http://css3-mediaqueries-js.googlecode.com/svn/trunk/css3-mediaqueries.js"></script>
+	<![endif]-->
+	
+
+	<!-- Optional theme -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+
+
+
+	<?php wp_head(); ?>
+    <?php zilla_head(); ?>
+	
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+
+   	<!-- Latest compiled and minified JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.2.0/js/tooltip.js"></script>
+	
+<!-- END head -->
+</head>
+
+<!-- BEGIN body -->
+<body <?php body_class('no-js'); ?>>
+
+
+	<!-- Google Tag Manager -->
+	<noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-PG8W3R"
+	height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+	'//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+	})(window,document,'script','dataLayer','GTM-PG8W3R');</script>
+	<!-- End Google Tag Manager -->
+
+    <?php zilla_body_start(); ?>
+
+	
+	<?php if( !is_page_template('template-portfolio.php') && !is_singular('portfolio') && !is_post_type_archive( 'portfolio' ) ) { ?>
+		<!--BEGIN #content -->
+		<div id="content" class="clearfix">
+	<?php } ?>
+
+	<?php zilla_content_start(); ?>
