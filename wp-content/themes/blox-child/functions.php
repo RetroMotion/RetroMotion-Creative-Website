ce19727e619bdbf86c59f666c05440ccaeb99021
Ti <tikawamoto@gmail.com>
2016-05-26 12:00:31 -0400

    blog index image size

--- wp-content/themes/blox-child/functions.php
+++ wp-content/themes/blox-child/functions.php
@@ -21,3 +21,47 @@ function theme_name_scripts() {
 add_action( 'wp_enqueue_scripts', 'theme_name_scripts' );
 
 add_filter("gform_confirmation_anchor", create_function("","return true;"));
+
+if ( !function_exists( 'zilla_theme_setup' ) ) {
+    function zilla_theme_setup() {
+        
+        /* Load translation domain --------------------------------------------------*/
+    	load_theme_textdomain( 'zilla', get_template_directory() . '/languages' );
+        
+        $locale = get_locale();
+    	$locale_file = get_template_directory() . "/languages/$locale.php";
+    	if ( is_readable( $locale_file ) )
+    		require_once( $locale_file );
+    		
+    	/* Register WP 3.0+ Menus ---------------------------------------------------*/
+ 		register_nav_menu( 'primary-menu', __('Primary Menu', 'zilla') );
+        register_nav_menu( 'footer-menu', __('Footer Menu', 'zilla') );
+   	
+    	/* Configure WP 2.9+ Thumbnails ---------------------------------------------*/
+    	add_theme_support( 'post-thumbnails' );
+    	set_post_thumbnail_size( 50, 50, true ); // Normal post thumbnails
+        add_image_size( 'blog-index', 800, '', true); // blog index/single pages
+        add_image_size( 'portfolio-full', 940, '', true); // portfolio full width
+        add_image_size( 'portfolio-index', 600, '', true); // portfolio index/single pages
+        add_image_size( 'portfolio-thumb', 188, 144, true); // portfolio thumbnails
+    	add_image_size( 'portfolio-admin-thumb', 35, 35, true ); // Used in the portfolio edit page
+    	
+    	/* Add support for post formats ---------------------------------------------*/
+        add_theme_support( 
+            'post-formats', 
+            array(
+                'aside',
+                'gallery',
+                'image',
+                'link',
+                'quote',
+                'video',
+                'audio'
+            ) 
+        );
+
+        add_theme_support( 'automatic-feed-links' );       
+    	
+    }
+}
+add_action( 'after_setup_theme', 'zilla_theme_setup' );
