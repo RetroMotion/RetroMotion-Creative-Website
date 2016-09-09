c31c88aee0ca8cedfc0e30822fd84f9889680aac
Ti <tikawamoto@gmail.com>
2016-09-09 12:47:24 -0400

    Fort Awesome

--- wp-content/themes/blox-child/header.php
+++ wp-content/themes/blox-child/header.php
@@ -9,18 +9,19 @@
 	<!-- Meta Tags -->
 	<meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	
+
 	<!-- Title -->
 	<title><?php wp_title('|', true, 'right'); ?></title>
-	
+
 	<!-- RSS & Pingbacks -->
 	<link rel="alternate" type="application/rss+xml" title="<?php bloginfo( 'name' ); ?> RSS Feed" href="<?php if(zilla_get_option('general_feedburner_url')){ echo zilla_get_option('general_feedburner_url'); } else { bloginfo( 'rss2_url' ); } ?>" />
 	<link href='https://fonts.googleapis.com/css?family=Roboto:400,100,100italic,300,300italic,400italic,500,500italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
 	<link href='https://fonts.googleapis.com/css?family=Sanchez:400,400italic' rel='stylesheet' type='text/css'>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/material-design-iconic-font/2.2.0/css/material-design-iconic-font.min.css">
+	<script src="https://use.fortawesome.com/3d08ce6f.js"></script>
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
-	
+
 	<!--favicon -->
 	<link rel="apple-touch-icon" sizes="57x57" href="/apple-icon-57x57.png">
 	<link rel="apple-touch-icon" sizes="60x60" href="/apple-icon-60x60.png">
@@ -40,12 +41,12 @@
 	<meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
 	<meta name="msapplication-navbutton-color" content="#f57f25">
 	<meta name="theme-color" content="#f57f25">
-	
+
 
 	<!--[if lt IE 9]>
 	<script src="http://css3-mediaqueries-js.googlecode.com/svn/trunk/css3-mediaqueries.js"></script>
 	<![endif]-->
-	
+
 
 	<!-- Optional theme -->
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
@@ -54,7 +55,7 @@
 
 	<?php wp_head(); ?>
     <?php zilla_head(); ?>
-	
+
 	<!-- Latest compiled and minified CSS -->
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 
@@ -62,16 +63,16 @@
    	<!-- Latest compiled and minified JavaScript -->
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.2.0/js/tooltip.js"></script>
-	
-		
 
-	
+
+
+
 <!-- END head -->
 </head>
 
 <!-- BEGIN body -->
 <body <?php body_class('no-js'); ?>>
-	
+
 	<!-- Google Tag Manager -->
 	<noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-PG8W3R"
 	height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
@@ -83,14 +84,14 @@
 	<!-- End Google Tag Manager -->
 
     <?php zilla_body_start(); ?>
-	
+
     <div class="header-outer">
-			
+
 	    <?php zilla_header_before(); ?>
 		<!-- BEGIN #header -->
 		<div id="header" class="clearfix">
 		<?php zilla_header_start(); ?>
-			
+
 			<!-- BEGIN #logo -->
 			<div id="logo">
 				<?php /*
@@ -106,21 +107,21 @@
 				<?php } ?>
 			<!-- END #logo -->
 			</div>
-			
+
 			<?php zilla_nav_before(); ?>
 			<?php if( has_nav_menu( 'primary-menu' ) ) {
-			    wp_nav_menu( array( 'theme_location' => 'primary-menu', 'container' => 'div', 'container_id' => 'primary-nav', 'menu_id' => 'primary-menu', 'menu_class' => 'sf-menu' ) ); 
+			    wp_nav_menu( array( 'theme_location' => 'primary-menu', 'container' => 'div', 'container_id' => 'primary-nav', 'menu_id' => 'primary-menu', 'menu_class' => 'sf-menu' ) );
 			} ?>
 			<?php zilla_nav_after(); ?>
-			
-		<?php zilla_header_end(); ?>	
+
+		<?php zilla_header_end(); ?>
 		<!--END #header-->
 		</div>
 		<?php zilla_header_after(); ?>
 		<div class="home-header-bg">
 		<!--BEGIN .page-header -->
 		<div class="page-header">
-			<?php 
+			<?php
 				$page_title = zilla_get_page_title();
 				$page_caption = html_entity_decode( zilla_get_page_caption() );
 
@@ -136,7 +137,7 @@
 	<!--END .header-outer -->
 	</div>
 	</div>
-	
+
 	<?php if( !is_page_template('template-portfolio.php') && !is_singular('portfolio') && !is_post_type_archive( 'portfolio' ) ) { ?>
 		<!--BEGIN #content -->
 		<div id="content" class="clearfix">
