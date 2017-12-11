22e71cc0938cde004801654a086423ee45a96272
Ti <tikawamoto@gmail.com>
2017-12-11 13:54:56 -0500

    SSP

--- wp-content/themes/blox-child/functions.php
+++ wp-content/themes/blox-child/functions.php
@@ -67,3 +67,8 @@ function zilla_theme_setup () {
 }
 
 /* jQuery ------*/
+/* SSP */
+add_filter( 'ssp_episode_download_link', 'ssp_use_raw_audio_file_url', 10, 3 );
+function ssp_use_raw_audio_file_url ( $url, $episode_id, $file ) {
+  return $file;
+}
